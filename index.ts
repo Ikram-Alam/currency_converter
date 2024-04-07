9eaa474af0e62022294693fecf4aed50e9d81d3f
Ikram Alam <ikramalam515@gmail.com>
2024-04-07 14:41:27 +0500

    version 1.0.1

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency : any = {
+    USD: 1, // base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280.32,
+    POU: 0.42,
+    YEN: 2.8,
+    RUB: 52.36
+}
+
+let userAnswer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "usercurrency",
+        message: "What is your currency : ",
+        choices: [ 'USD', 'EUR', 'GBP', 'INR', 'PKR', 'POU', 'YEN', 'RUB' ]
+    },
+    {
+        type: "list",
+        name: "convert",
+        message: "What do you want to convert your currency : ",
+        choices: [ 'USD', 'EUR', 'GBP', 'INR', 'PKR', 'POU', 'YEN', 'RUB' ]
+    },
+    {
+        name: "amount",
+        message: "What is your amount : ",
+        type: "number",
+    }
+  ]
+)
+
+let userCurrency  = currency[userAnswer.usercurrency];
+let userConvert   = currency[userAnswer.convert];
+let amount = userAnswer.amount;
+let baseAmount = amount / userCurrency;
+let convertedAmount = baseAmount * userConvert;
+
+console.log(convertedAmount)
